ea770547997920212dbd862087d5d05f2dffff92
Tanner Davison <Tboydavison@gmail.com>
2024-04-10 11:40:39 -0600

    added blogpost filtering for all blogposts

--- src/components/MyBlogs.js
+++ src/components/MyBlogs.js
@@ -9,29 +9,57 @@ const MyBlogs = () => {
   const[blogposts, setBlogposts] = useState([])
 
 useEffect(()=>{
+
   const getBlogs = async ()=>{
+
     const blogCollection = collection(db,"users");
     const allUsersData = await getDocs(blogCollection);
         const allDataFromUsers = allUsersData.docs.map((doc)=>({
           ...doc.data(),
           id: doc.id,
         }))
-        // console.log('AllUsersData', allDataFromUsers)
+        setBlogposts(allDataFromUsers)
         
   }
-  getBlogs()
- console.log(blogposts);
 
-},[blogposts])
+  getBlogs()
+
+},[])
+
+const mappedBlogs = blogposts.map((post, index) => {
+  let posts = post.blogposts && post.blogposts.length > 0 ? post.blogposts : null;
+  let mappedPosts = posts && posts.map((post, index) => {
+    return (
+      <Blogs key={index}>{post}</Blogs>
+    );
+  });
+  
+  return (
+    <Blogs>{mappedPosts}</Blogs>
+  );
+});
+
   return (
     <Wrapper>
-      
+      <Blogs>
+        {mappedBlogs}
+        </Blogs>
     </Wrapper>
   )
 }
 
 export default MyBlogs
+const Blogs = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+width:300px;
+height: 300px;
+color:black;
+${text.bodyM}
 
+`
 const Wrapper = styled.div`
 display:flex;
 flex-direction: column;
