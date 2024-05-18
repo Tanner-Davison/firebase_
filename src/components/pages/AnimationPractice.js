48f64a7dc750b44966ae7e128d47bcdef8686787
Tanner Davison <Tboydavison@gmail.com>
2024-05-18 15:56:06 -0600

    lets go

--- src/components/pages/AnimationPractice.js
+++ src/components/pages/AnimationPractice.js
@@ -1,148 +1,90 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import gsap from "gsap";
 import text from "styles/text";
+import OrderedItemsAnimation from "./OrderedItemsAnimation";
+import MouseFollower from "./MouseFollower";
 
 const AnimationPractice = () => {
-  const clickCountRef = useRef(0); 
-  const lastClickedRef = useRef(null); 
+    let animationArray=[{Component:<OrderedItemsAnimation/>, animeName:'Ordered Items'},{Component:<MouseFollower/>, animeName: 'Cursor Painter'}]
+    const [visibleComponent, setVisibleComponent]=useState(animationArray[0].Component)
+const animeRef = useRef(0);
 
-  const handleMouseOver = (e, index) => {
-    const movement = 150 - (index + 1) + 'px'; 
-    gsap.to(e.currentTarget, { x: movement, ease:'power1.out' });
-  };
-
-  const handleMouseLeave = (e) => {
-    clickCountRef.current = 0;
-    gsap.to(e.currentTarget, { zIndex:0,x: 0,width:'300px',height:'300px', ease:'power1.in' });
-  };
-
-  const handleClick = (e) => {
-    const clickedElement = e.currentTarget;
-    
-    if (lastClickedRef.current === clickedElement) {
-        gsap.to(lastClickedRef.current,{zIndex:0});
-        gsap.to(lastClickedRef.current,{x: 0,width:'300px',height:'300px', ease: 'power1.out' })
-      clickCountRef.current = 0;
-      lastClickedRef.current = clickedElement;
-    }
-    clickCountRef.current += 1;
-    if(clickCountRef.current === 2){
-        gsap.to(e.currentTarget,{zIndex:0});
-        gsap.to(e.currentTarget, { x: 0,width:'300px',height:'300px', ease: 'power1.in' });
-        return clickCountRef.current = 0
-    }else{
-        gsap.to(e.currentTarget, {zIndex:5, width:'400px',height:'400px',ease:'power1.inOut'})
-    }
-
-  };
-
-  useEffect(() => {
- 
-    const items = document.querySelectorAll(".box");
-
-    items.forEach((box, index) => {
-      box.addEventListener("mouseenter", (e) => handleMouseOver(e, index));
-      box.addEventListener("mouseleave", handleMouseLeave);
-      box.addEventListener("click", handleClick);
-    });
-
-    return () => {
-      items.forEach((box, index) => {
-        box.removeEventListener("mouseenter", (e) => handleMouseOver(e, index));
-        box.removeEventListener("mouseleave", handleMouseLeave);
-        box.removeEventListener("click", handleClick);
-      });
-    };
-  }, []);
-
-  return (
-    <Wrapper>
-      <Controls></Controls>
-      <Slider className={"slider"}>
-        <Item className={"box box-6"}>6</Item>
-        <Item className={"box box-5"}>5</Item>
-        <Item className={"box box-4"}>4</Item>
-        <Item className={"box box-3"}>3</Item>
-        <Item className={"box box-2"}>2</Item>
-        <Item className={"box box-1"}>1</Item>
-      </Slider>
-    </Wrapper>
-  );
-};
+const handleRightClick =()=>{
+animeRef.current += 1
+setVisibleComponent(animationArray[animeRef.current].Component)
+}
+const handleLeftClick=()=>{
+    animeRef.current -= 1
+setVisibleComponent(animationArray[animeRef.current].Component)
+}
+    return(
+        <Wrapper>
+            <Controls>
+                <Left onClick={handleLeftClick}>Left</Left>
+                <ComponentName>{animationArray[animeRef.current].animeName}</ComponentName>
+                <Right onClick={handleRightClick}>Right</Right>
+            </Controls>
+            <ViewBox>        
+            {animationArray[animeRef.current]?.Component}
+            </ViewBox>
+        </Wrapper>
+    )
+}
 
 export default AnimationPractice;
 
-const Item = styled.div`
-  ${text.bodyMBold}
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 25px;
-  width: 300px;
-  height: 300px;
-  -webkit-box-shadow: 10px 9px 11px -5px #000000;
-  box-shadow: 10px 9px 11px -5px #000000;
+const ViewBox = styled.div`
+position: relative;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: black;
+border-radius: 25px;
+width:80%;
+outline:4px solid red;
+height: 65vh;
+`
+const ComponentName = styled.p`
+${text.bodyMBold}
+`
 
-  &.box-1 {
-    background-color: hsla(246, 46%, 37%, 1);
-  }
-  &.box-2 {
-    background-color: hsla(239, 77%, 70%, 1);
-  }
-  &.box-3 {
-    background-color: hsla(45, 99%, 49%, 1);
-  }
-  &.box-4 {
-    background-color: hsla(34, 99%, 47%, 1);
-  }
-  &.box-5 {
-    background-color: hsla(22, 97%, 48%, 1);
-  }
-  &.box-6 {
-    background-color: hsla(33, 88%, 32%, 1);
-  }
-  &.box:nth-child(6) {
-    top: 0;
-    left: -50px;
-  }
-  &.box:nth-child(5) {
-    top: -30px;
-    left: -10px;
-  }
-  &.box:nth-child(4) {
-    top: -50px;
-    left: 30px;
-  }
-  &.box:nth-child(3) {
-    top: -70px;
-    left: 70px;
-  }
-  &.box:nth-child(2) {
-    top: -90px;
-    left: 110px;
-  }
-  &.box:nth-child(1) {
-    top: -110px;
-    left: 150px;
-  }
-`;
-
-const Slider = styled.div`
-  position: relative;
-  width: 300px;
-  height: 200px;
-  perspective: 100px;
-`;
-
+const Right = styled.button`
+${text.bodyM}
+width:5.903vw;
+border:0.139vw outset black;
+background-color: white;
+border-radius: 1.736vw;
+transition:transform .3s ease-in-out;
+&:hover{
+    transform:scale(1.09);
+}
+`
+const Left = styled.button`
+${text.bodyM}
+width:5.903vw;
+border:0.139vw outset black;
+background-color: white;
+border-radius: 1.736vw;
+transition:transform .3s ease-in-out;
+&:hover{
+    transform:scale(1.09);
+}
+`
 const Controls = styled.div`
   position: relative;
   display: flex;
-  margin: 50px 0px 185px 0px;
-  width: 400px;
-  height: 100px;
-  outline: 2px solid red;
+  background-color: hsla(246, 46%, 37%, 1);
+  color:white;
+ justify-content: space-between;
+ align-items: center;
+ width: auto;
+ gap:25px;
+ padding:0px 25px;
+ height: 100px;
+ margin: 50px 0px 50px 0px;
+ border-radius:25px;
+ border:2px inset silver;
 `;
 
 const Wrapper = styled.div`
@@ -150,5 +92,4 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   height: 100vh;
-  cursor: pointer;
 `;
---
+++ src/components/pages/OrderedItemsAnimation.js
@@ -0,0 +1,143 @@
+import React, {useEffect, useRef} from 'react'
+import styled from 'styled-components';
+import media from 'styles/media';
+import colors from 'styles/colors';
+import text from 'styles/text';
+import gsap from 'gsap';
+
+const OrderedItemsAnimation = () => {
+    const clickCountRef = useRef(0); 
+    const lastClickedRef = useRef(null); 
+  
+    const handleMouseOver = (e, index) => {
+      const movement = 150 - (index + 1) + 'px'; 
+      gsap.to(e.currentTarget, { x: movement, ease:'power1.out' });
+    };
+  
+    const handleMouseLeave = (e) => {
+      clickCountRef.current = 0;
+      gsap.to(e.currentTarget,{zIndex:0});
+      gsap.to(e.currentTarget, { zIndex:0,x: 0,width:'300px',height:'300px', ease:'power1.in' });
+    };
+  
+    const handleClick = (e) => {
+     
+      const clickedElement = e.currentTarget;
+      
+      if (lastClickedRef.current === clickedElement) {
+          gsap.to(lastClickedRef.current,{zIndex:0});
+          gsap.to(lastClickedRef.current,{x: 0,width:'300px',height:'300px', ease: 'power1.out' })
+        clickCountRef.current = 0;
+        lastClickedRef.current = clickedElement;
+      }
+      clickCountRef.current += 1;
+      if(clickCountRef.current === 2){
+          gsap.to(e.currentTarget,{zIndex:0});
+          gsap.to(e.currentTarget, { x: 0,width:'300px',height:'300px', ease: 'power1.in' });
+          return clickCountRef.current = 0
+      }else{
+          gsap.to(e.currentTarget, {zIndex:5, width:'400px',x:'-50px',height:'400px',ease:'power1.inOut'})
+      }
+  
+    };
+  
+    useEffect(() => {
+   
+      const items = document.querySelectorAll(".box");
+  
+      items.forEach((box, index) => {
+        box.addEventListener("mouseenter", (e) => handleMouseOver(e, index));
+        box.addEventListener("mouseleave", handleMouseLeave);
+        box.addEventListener("click", handleClick);
+      });
+  
+      return () => {
+        items.forEach((box, index) => {
+          box.removeEventListener("mouseenter", (e) => handleMouseOver(e, index));
+          box.removeEventListener("mouseleave", handleMouseLeave);
+          box.removeEventListener("click", handleClick);
+        });
+      };
+    }, []);
+  
+    return (
+ 
+        
+        <Slider className={"slider"}>
+          <Item className={"box box-6"}>6</Item>
+          <Item className={"box box-5"}>5</Item>
+          <Item className={"box box-4"}>4</Item>
+          <Item className={"box box-3"}>3</Item>
+          <Item className={"box box-2"}>2</Item>
+          <Item className={"box box-1"}>1</Item>
+        </Slider>
+   
+    );
+  };
+export default OrderedItemsAnimation
+
+const Item = styled.div`
+  ${text.bodyMBold}
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  width: 300px;
+  height: 300px;
+  cursor: pointer;
+  -webkit-box-shadow: 10px 9px 11px -5px #000000;
+  box-shadow: 10px 9px 11px -5px #000000;
+
+  &.box-1 {
+    background-color: hsla(246, 46%, 37%, 1);
+  }
+  &.box-2 {
+    background-color: hsla(239, 77%, 70%, 1);
+  }
+  &.box-3 {
+    background-color: hsla(45, 99%, 49%, 1);
+  }
+  &.box-4 {
+    background-color: hsla(34, 99%, 47%, 1);
+  }
+  &.box-5 {
+    background-color: hsla(22, 97%, 48%, 1);
+  }
+  &.box-6 {
+    background-color: hsla(33, 88%, 32%, 1);
+  }
+  &.box:nth-child(6) {
+    top: 0;
+    left: -50px;
+  }
+  &.box:nth-child(5) {
+    top: -30px;
+    left: -10px;
+  }
+  &.box:nth-child(4) {
+    top: -50px;
+    left: 30px;
+  }
+  &.box:nth-child(3) {
+    top: -70px;
+    left: 70px;
+  }
+  &.box:nth-child(2) {
+    top: -90px;
+    left: 110px;
+  }
+  &.box:nth-child(1) {
+    top: -110px;
+    left: 150px;
+  }
+`;
+
+const Slider = styled.div`
+  position: relative;
+  width: 300px;
+  height: 200px;
+  perspective: 100px;
+`;
+
+
